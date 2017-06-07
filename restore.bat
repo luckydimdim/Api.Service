37553859524803c3ba6b811f960769064c86adbd
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-06-07 12:54:15 +0300

    restore.bat

---
+++ restore.bat
@@ -0,0 +1,12 @@
+set project_name=Web
+set configuration=Release
+set nuget_source=http://cm-ylng-msk-04/nuget/nuget
+
+for /f %%G in ('dir %UserProfile%\.nuget\packages\cmas.* /a:d /b') do @(
+   rd /s /q %UserProfile%\.nuget\packages\%%G
+)
+
+dotnet clean %project_name%\%project_name%.csproj --configuration %configuration%
+dotnet restore %project_name%\%project_name%.csproj --source %nuget_source% --no-cache
+
+pause
